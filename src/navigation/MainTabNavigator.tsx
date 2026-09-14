cd7178a009188fdc4d7efbcb526264d5af50807e
takeharanaoki <devadeepmind@gmail.com>
2026-09-15 06:56:12 +0900

    fixed error

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -1,55 +1,101 @@
 import ChatsScreen from '@/components/screens/ChatsScreens';
 import EmptyView from '@/components/screens/EmptyView';
-import { Entypo, Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import type { NativeBottomTabIcon } from '@react-navigation/bottom-tabs/unstable';
+import {
+  createNativeBottomTabNavigator,
+} from '@react-navigation/bottom-tabs/unstable';
+import { Platform } from 'react-native';
+import type { SFSymbols7_0 } from 'sf-symbols-typescript';
 
-const Tab = createBottomTabNavigator();
+const Tab = createNativeBottomTabNavigator();
+
+const getTabIcon = (
+  iosName: SFSymbols7_0,
+  androidName: string,
+) => (): NativeBottomTabIcon => Platform.OS === 'ios'
+  ? {
+    type: 'sfSymbol',
+    name: iosName,
+  }
+  : {
+    type: 'materialSymbol',
+    name: androidName,
+  } as unknown as NativeBottomTabIcon;
 
 const MainTabNavigator = () => {
   return (
-    <Tab.Navigator 
-    initialRouteName="Chats"
+    <Tab.Navigator
+      initialRouteName="Chats"
     >
-
-      <Tab.Screen 
-      name="Status" 
-      component={EmptyView} 
-      options={{ tabBarIcon: ({color, size}) => <Ionicons name="person" size={size} color={color} /> }}
+      <Tab.Screen
+        name="Status"
+        component={EmptyView}
+        options={{
+          title: 'Status',
+          tabBarIcon: getTabIcon(
+            'person',
+            'person'
+          ),
+        }}
       />
 
-      <Tab.Screen 
-      name="Calls" 
-      component={EmptyView} 
-      options={{ tabBarIcon: ({color, size}) => <Ionicons name="call-outline" size={size} color={color} /> }}
+      <Tab.Screen
+        name="Calls"
+        component={EmptyView}
+        options={{
+          title: 'Calls',
+          tabBarIcon: getTabIcon(
+            'phone',
+            'call',
+          )
+        }}
       />
 
-      <Tab.Screen 
-      name="Camera" 
-      component={EmptyView} 
-      options={{ tabBarIcon: ({color, size}) => <Ionicons name="camera" size={size} color={color} /> }}
+      <Tab.Screen
+        name="Camera"
+        component={EmptyView}
+        options={{
+          title: 'Camera',
+          tabBarIcon: getTabIcon(
+            'camera',
+            'photo_camera',
+          )
+        }}
       />
 
-      <Tab.Screen 
-      name="Chats" 
-      component={ChatsScreen} 
-      options={({ navigation }) => ({ 
-        tabBarIcon: ({color, size}) => (<Ionicons name="person" size={size} color={color} />),
-        headerRight: () => (
-          <Entypo onPress={() => navigation.navigate('Contacts')} name="new-message" size={18} color={'royalblue'} style={{ marginRight: 10 }} />
-        )
-        
-       })}
+      <Tab.Screen
+        name="Chats"
+        component={ChatsScreen}
+        options={({ navigation }) => ({
+          title: 'Chats',
+          tabBarIcon: getTabIcon(
+            'message',
+            'chat',
+          ),
+          headerRight: () => (
+            <Entypo
+              onPress={() => navigation.navigate('Contacts')}
+              name="new-message"
+              size={18}
+              color="royalblue"
+              style={{ marginRight: 10 }}
+            />
+          ),
+        })}
       />
 
-      <Tab.Screen 
-      name="Settings" 
-      component={EmptyView} 
-      options={{ 
-        tabBarIcon: ({color, size}) => 
-          (<Ionicons name="settings-outline" size={size} color={color} /> ),
-      }}
+      <Tab.Screen
+        name="Settings"
+        component={EmptyView}
+        options={{
+          title: 'Settings',
+          tabBarIcon: {
+            type: 'sfSymbol',
+            name: 'gearshape',
+          },
+        }}
       />
-
     </Tab.Navigator>
   );
 };
